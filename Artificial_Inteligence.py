da26b138997a8af2e6148265136a165b73f135e7
Sklyvan <37016233+Sklyvan@users.noreply.github.com>
2019-04-12 23:44:12 +0200

    Update Artificial_Inteligence.py

--- Artificial_Inteligence.py
+++ Artificial_Inteligence.py
@@ -4,6 +4,4 @@ import random
 class RandomIA:
 
     def __init__(self, nRows, nColumns, GameMatrix):
-
-        if GameMatrix.isEmpty is True:
-            print("Ficha en el Centro.")
+        raise NotImplementedError
